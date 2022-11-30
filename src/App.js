856e898f849a9aa8c957631d3debae533f519c3a
Akhmed Magomedov <akhma96@gmail.com>
2022-12-01 00:07:49 +0300

    add state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import { useState } from 'react';
 import './index.scss';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const handlerPlus = () => setCount(count + 1);
+
+
+  const handlerMinus = () => setCount(count - 1);
+
+
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{count}</h1>
+        <button onClick={handlerMinus} className="minus">- Минус</button>
+        <button onClick={handlerPlus} className="plus">Плюс +</button>
       </div>
     </div>
   );
