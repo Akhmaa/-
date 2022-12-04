e02c4ab2c95760c5fa503b00f631dc7bc0afbfcd
Akhmed Magomedov <akhma96@gmail.com>
2022-12-04 17:15:42 +0300

    use  useRef

--- src/App.js
+++ src/App.js
@@ -1,12 +1,64 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import { useEffect } from 'react';
 import { Block } from './Block';
+import json from './currency.json';
 import './index.scss';
 
 function App() {
+  const ratesRef = useRef({});
+
+  const [fromCurrency, setFromCurrency] = useState('RUB');
+  const [toCurrency, setToCurrency] = useState('USD');
+  const [fromPrici, setFromPrice] = useState(0);
+  const [toPrice, setToPrice] = useState(1);
+
+
+
+  //должна была быть ссылка но сайт больше не действителен и приглось заменить на локальный файл,
+  //useEffect просто остался
+  useEffect(() => {
+    ratesRef.current = json.rates;
+    onChangeToPrice(1)
+  }, []);
+
+
+  const onChangeFromPrice = (value) => {
+    const price = value / ratesRef.current[fromCurrency];
+    const result = price * ratesRef.current[toCurrency];
+    setToPrice(result);
+    setFromPrice(value)
+  }
+
+  const onChangeToPrice = (value) => {
+    const price = value / ratesRef.current[toCurrency];
+    const result = price * ratesRef.current[fromCurrency];
+    setFromPrice(result)
+    setToPrice(value)
+  }
+
+  useEffect(() => {
+    onChangeFromPrice(fromPrici);
+  }, [fromCurrency]);
+
+  useEffect(() => {
+    onChangeToPrice(toPrice)
+  }, [toCurrency])
+
+
   return (
     <div className="App">
-      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
-      <Block value={0} currency="USD" />
+      <Block
+        value={fromPrici}
+        currency={fromCurrency}
+        onChangeCurrency={setFromCurrency}
+        onChangeValue={onChangeFromPrice}
+      />
+      <Block
+        value={toPrice}
+        currency={toCurrency}
+        onChangeCurrency={setToCurrency}
+        onChangeValue={onChangeToPrice}
+      />
     </div>
   );
 }
